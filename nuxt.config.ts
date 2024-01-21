6a06d10456117657c477fdf9a456cf8c08c01ba6
evanlanglais@gmail.com <Evan Langlais>
2024-01-20 18:08:42 -0800

    idk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-  modules: [
-    "@nuxtjs/supabase",
-  ],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/eslint-module"],
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL || "http://localhost:3000",
     },
-  },
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
   },
   supabase: {
     redirectOptions: {
@@ -20,4 +15,14 @@ export default defineNuxtConfig({
     },
   },
   extends: ["@nuxt-awesome/theme"],
+  nitro: {
+    azure: {
+      config: {
+        // ...
+        platform: {
+          apiRuntime: "node:18",
+        },
+      },
+    },
+  },
 });

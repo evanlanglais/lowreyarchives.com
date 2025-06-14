4ba628219153630c5150beeb25260603b711b2e1
evanlanglais@gmail.com <Evan Langlais>
2025-06-14 16:03:16 -0700

    feat(cloudflare): sign stream urls locally

---
+++ server/utils/cloudflare-utils.ts
@@ -0,0 +1,50 @@
+export async function streamSignedUrl(videoUID: string): Promise<string> {
+  const runtimeConfig = useRuntimeConfig();
+  const encoder = new TextEncoder();
+  const expiresIn = Math.floor(Date.now() / 1000) + 2 * 60 * 60;
+  const headers = {
+    alg: "RS256",
+    kid: runtimeConfig.cloudflareStreamKeyId,
+  };
+  const data = {
+    sub: videoUID,
+    kid: runtimeConfig.cloudflareStreamKeyId,
+    exp: expiresIn,
+  };
+
+  const token = `${objectToBase64url(headers)}.${objectToBase64url(data)}`;
+
+  const jwk = JSON.parse(atob(runtimeConfig.cloudflareStreamKeyJwt));
+
+  const key = await crypto.subtle.importKey(
+    "jwk",
+    jwk,
+    {
+      name: "RSASSA-PKCS1-v1_5",
+      hash: "SHA-256",
+    },
+    false,
+    ["sign"],
+  );
+
+  const signature = await crypto.subtle.sign(
+    { name: "RSASSA-PKCS1-v1_5" },
+    key,
+    encoder.encode(token),
+  );
+
+  return `${token}.${arrayBufferToBase64Url(signature)}`;
+}
+
+export function arrayBufferToBase64Url(buffer) {
+  return btoa(String.fromCharCode(...new Uint8Array(buffer)))
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+}
+
+export function objectToBase64url(payload) {
+  return arrayBufferToBase64Url(
+    new TextEncoder().encode(JSON.stringify(payload)),
+  );
+}

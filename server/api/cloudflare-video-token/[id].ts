ad0d8785b631f4a7e448c31eac063b213565b16d
Evan Langlais <evanlanglais@gmail.com>
2025-06-14 16:22:38 -0700

    feat(cloudflare): sign stream urls locally (#10)
    Co-authored-by: evanlanglais@gmail.com <Evan Langlais>

--- server/api/cloudflare-video-token/[id].ts
+++ server/api/cloudflare-video-token/[id].ts
@@ -1,4 +1,4 @@
-import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server";
+import { streamSignedUrl } from "~~/server/utils/cloudflare-utils";
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
@@ -10,37 +10,5 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const runtimeConfig = useRuntimeConfig();
-
-  // Get signed URL
-  try {
-    const tokenResponse = await $fetch(
-      `https://api.cloudflare.com/client/v4/accounts/${runtimeConfig.cloudflareAccount}/stream/${id}/token`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${runtimeConfig.cloudflareKey}`,
-          "content-type": "application/json;charset=UTF-8",
-        },
-        body: JSON.stringify({
-          exp: Math.floor(Date.now() / 1000) + 2 * 60 * 60,
-        }),
-      },
-    );
-
-    if (!tokenResponse.success) {
-      throw createError({
-        statusCode: 500,
-        statusMessage: "Unable to get access token",
-      });
-    }
-
-    return tokenResponse.result.token; // `https://customer-${runtimeConfig.cloudflareStreamCode}.cloudflarestream.com/${tokenResponse.result.token}/manifest/video.m3u8`;
-  } catch (error) {
-    console.error(error);
-    throw createError({
-      statusCode: 500,
-      statusMessage: `Unable to get cloudflare video url`,
-    });
-  }
+  return await streamSignedUrl(id);
 });

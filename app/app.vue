e294d87aa88b3e07e85efee9658bb98daaf28cca
evanlanglais@gmail.com <Evan Langlais>
2025-06-22 22:48:30 -0700

    more bug fix

--- app/app.vue
+++ app/app.vue
@@ -16,30 +16,6 @@ import SiteHeader from "~/components/SiteHeader.vue";
           Copyright © {{ new Date().getFullYear() }} Evan Langlais
         </p>
       </template>
-
-      <template #right>
-        <!--      <UButton-->
-        <!--        to="https://x.com/nuxt_js"-->
-        <!--        target="_blank"-->
-        <!--        icon="i-simple-icons-x"-->
-        <!--        color="gray"-->
-        <!--        variant="ghost"-->
-        <!--      />-->
-        <!--      <UButton-->
-        <!--        to="https://discord.com/invite/ps2h6QT"-->
-        <!--        target="_blank"-->
-        <!--        icon="i-simple-icons-discord"-->
-        <!--        color="gray"-->
-        <!--        variant="ghost"-->
-        <!--      />-->
-        <!--      <UButton-->
-        <!--        to="https://github.com/nuxt/nuxt"-->
-        <!--        target="_blank"-->
-        <!--        icon="i-simple-icons-github"-->
-        <!--        color="gray"-->
-        <!--        variant="ghost"-->
-        <!--      />-->
-      </template>
     </UFooter>
   </UApp>
 </template>
